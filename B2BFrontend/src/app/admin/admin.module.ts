26d049e6e3e8e4afaa19a629577d835006a7a300
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-01-22 00:28:12 +0300

    frontende-baslandi

---
+++ B2BFrontend/src/app/admin/admin.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
+import { LayoutsModule } from './layouts/layouts.module';
+import { LoginModule } from './login/login.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeModule,
+    LayoutsModule,
+    LoginModule
+  ],
+  exports: [
+    HomeModule,
+    LayoutsModule,
+    LoginModule
+  ]
+})
+export class AdminModule { }
---
+++ B2BFrontend/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './admin/home/home.component';
+import { LayoutsComponent } from './admin/layouts/layouts.component';
+import { LoginComponent } from './admin/login/login.component';
+
+const routes: Routes = [
+  {
+    path: "admin-login",
+    component: LoginComponent,
+    loadChildren: ()=> import("./admin/login/login.module").then(m=> m.LoginModule)
+  },
+  {
+    path: "admin",
+    component: LayoutsComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent,
+        loadChildren: ()=> import('./admin/home/home.module').then(m=> m.HomeModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
